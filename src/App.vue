bd92da4c7a0e658fee8eb9a94a3ce2caf0f288e0
Alejandro Rivas <jarlalejor@gmail.com>
2017-11-03 09:18:06 -0500

    jarl-social-vue

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  #app
+    h1 Hola Mundo
+    button.button.is-primary.is-large Button
+    jc-form
+</template>
+
+<script>
+import JcForm from '@/components/chat/Form.vue'
+
+export default {
+  name: 'app',
+  components:  {
+    JcForm
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import './scss/style.scss';
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/chat/Form.vue
@@ -0,0 +1,5 @@
+<template lang="pug">
+	form
+		input(placeholder='Escribetu mensaje')
+		button Enviar
+</template>
